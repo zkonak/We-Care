987008209aef67f4ef68de1781492adaa65feb6e
samira <houacine.samira@gmail.com>
2022-03-18 15:58:46 +0100

    mise en place de docker

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,38 +1,23 @@
-import { ConnectionOptions, getConnectionManager } from "typeorm";
+import { getConnectionManager } from "typeorm";
 import { Patient } from "../modules/Patient/entity";
 import { Doctor } from "../modules/Doctor/entity";
 import { Service } from "../modules/Service/entity";
-import config from "./env";
 import { Consultation } from "../modules/Consultation/entity";
 console.log("database is connected");
 const entities = [Patient, Doctor, Service, Consultation];
-const options: ConnectionOptions = {
-  type: "mysql",
-  host: config.db_host,
-  port: config.db_port,
-  username: config.db_user,
-  password: config.db_password,
-  database: config.db_name,
-  logging: true,
-  synchronize: true,
-  migrationsRun: false,
-  entities: [__dirname + "/../../modules/**/entity.{js,ts}"],
-  migrations: ["src/config/database/migration/*.ts"],
-};
+
 const connectionManager = getConnectionManager();
 
-// const db = connectionManager.create({
-//   type: "mysql",
-//   host: "127.0.0.1",
-//   port: 3306,
-//   username: "root",
-//   password: "root",
-//   database: "we_care",
-//   logging: true,
-//   synchronize: true,
-//   entities: entities,
-// });//
-
-const db = connectionManager.create(options);
+const db = connectionManager.create({
+  type: "mysql",
+  host: "127.0.0.1",
+  port: 3306,
+  username: "root",
+  password: "root",
+  database: "we_care",
+  logging: true,
+  synchronize: true,
+  entities: entities,
+});
 
 export default db;
